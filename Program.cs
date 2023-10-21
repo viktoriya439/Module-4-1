32a87ca8b01ed802e6f6a86356d11c1d2881adee
viktoriya439 <lukyanenkoviktoriya439@gmail.com>
2023-10-21 22:40:51 +0300

    4.1. Условные выражения и конструкции. Задание 4.1.17

--- Program.cs
+++ Program.cs
@@ -108,7 +108,82 @@
                 Console.WriteLine("Your color is cyan!");
             }
 
+            #region
+            /*int A;
+            int B;
+            double X;
+            double Y;
+            bool C = (A < B) | (X > Y);
+            
+            Console.WriteLine(C); 
+            Console.ReadKey();*/
 
+            /* var a = 5 + 6;
+             var b = 7 + 8;
+
+             var c = (b != a) & (b > a + 1);
+             var d = (b != a) && (b > a + 1);
+
+             Console.WriteLine(c);
+             Console.WriteLine(d);
+             Console.ReadKey();*/
+
+            /*var a = 6;
+            var b = 7;
+
+            var c = (a < b) ^ (a != b);
+
+            Console.WriteLine(c);
+            Console.ReadKey(); */
+
+
+
+            /*
+             var inv = true; 
+             var result = !inv;
+            Console.WriteLine(result);*/
+
+            /*
+            var a = 6;
+            var b = 7;
+
+            if (a == b)
+            {
+                Console.WriteLine("Условие истинно");
+            }
+            else
+            {
+                Console.WriteLine("Условие ложно");
+            } 
+            */
+
+            /*
+            var a = 6;
+            var b = 7;
+
+            if (a == b && b > 1)
+            {
+                Console.WriteLine("Условие истинно");
+            }
+
+            else if (b > 10 || b == 7)
+            {
+                Console.WriteLine("Значение b = {0} больше 10 или равно 7", b);
+            }
+            else
+            {
+                Console.WriteLine("Значение b = {0}", b);
+            }
+
+
+            var a = 6;
+            var b = 7;
+
+            var c = a != b ? a + b : b;
+
+            Console.WriteLine(c);
+            */
+            #endregion
         }
     }
 }
