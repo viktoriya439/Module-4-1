01ec32034ca7ba60fe28dbb7db7ede33d249b8fe
viktoriya439 <lukyanenkoviktoriya439@gmail.com>
2023-10-23 16:42:10 +0300

    цикл for

--- Program.cs
+++ Program.cs
@@ -32,7 +32,7 @@
 
                  Console.WriteLine("Your color is cyan!");
              }
-            */
+            
 
             Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
             var color = Console.ReadLine();
@@ -71,6 +71,43 @@
 
                     break;
 
+            }
+            */
+
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+            for (int i = 5; i > 1 ; i--)
+            {
+                Console.WriteLine("Iteration {0}", i);
+                switch (Console.ReadLine())
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is red!");
+                        break;
+
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is green!");
+                        break;
+
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is cyan!");
+                        break;
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is yellow!");
+                        break;
+                }
             }
         }
     }
